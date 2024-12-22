0b402c7d58597709d9f130b109f2adda922d8d3b
AathiEswar <aathi28082003@gmail.com>
2024-12-22 09:12:36 +0530

    intro improvised

--- src/fleetstudio/fleet-card/components/loader/LoadingScreen.jsx
+++ src/fleetstudio/fleet-card/components/loader/LoadingScreen.jsx
@@ -24,33 +24,33 @@ function LoadingScreen() {
       })
 
       gsap.set(".loader-text-container", { scale: 0.2 })
-      gsap.set('.tc1', { y: "33dvh" })
+      gsap.set('.tc1-text', { y: "33dvh" })
       gsap.set('.tc2', { y: 0, })
-      gsap.set('.tc3', { y: "-33dvh" })
+      gsap.set('.tc3-text', { y: "-33dvh" })
 
 
-      timelineText1.to('.tc1', {
+      timelineText1.to('.tc1-text', {
         y: 0,
         delay: 6,
       })
         .to(".tc1", {
           x: "-100dvw",
-          delay : 1,
+          delay: 0.5,
         })
 
       timelineText2
         .to(".tc2", {
           x: "100dvw",
-          delay: 8,
+          delay: 7.5,
         })
 
-      timelineText3.to('.tc3', {
+      timelineText3.to('.tc3-text', {
         y: 0,
         delay: 6,
       })
         .to('.tc3', {
           x: "-200dvw",
-          delay : 1,
+          delay: 0.5,
         })
 
       timeline
@@ -79,14 +79,20 @@ function LoadingScreen() {
       style={{ color: "var(--accent)" }}
     >
       <div className='w-full h-full bg-[transparent] flex flex-col justify-between items-center druk-text-heavy'>
-        <div className="flex justify-center items-center w-screen h-screen text-[25vw] sm:text[20dvw] md:text-[20dvw] leading-none overflow-hidden text-center text-cover tc1 bg-black">
-          AATHI ESWAR
+        <div className="z-10 flex justify-center items-center w-screen h-screen text-[25vw] sm:text[20dvw] md:text-[15dvw] leading-none text-center text-cover tc1 bg-black">
+          <p className='tc1-text z-10 w-full h-full'>
+            AATHI ESWAR
+          </p>
         </div>
-        <div className="flex justify-center items-center w-screen h-screen text-[25vw] sm:text[20dvw] md:text-[20dvw] leading-none overflow-hidden text-center bg-black tc2 z-10">
-          AATHI ESWAR
+        <div className="z-0 flex justify-center items-center w-screen h-screen text-[25vw] sm:text[20dvw] md:text-[15dvw] leading-none  text-center bg-black tc2">
+          <p className='z-0 tc2-text  w-full h-full'>
+            AATHI ESWAR
+          </p>
         </div>
-        <div className="flex justify-center items-center w-screen h-screen text-[25vw] sm:text[20dvw] md:text-[20dvw] leading-none overflow-hidden text-center text-cover tc3 bg-black">
-          AATHI ESWAR
+        <div className="z-10 flex justify-center items-center w-screen h-screen text-[25vw] sm:text[20dvw] md:text-[15dvw] leading-none text-center text-cover tc3 bg-black">
+          <p className='z-10 tc3-text  w-full h-full'>
+            AATHI ESWAR
+          </p>
         </div>
       </div>
     </div>
